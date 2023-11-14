678ce0076a6bf1c9ef88ff4b9354069bb8a2ed82
MarioNadalCasa <220056@iessanalberto.com>
2023-11-14 19:36:43 +0100

    14/11/2023

--- src/main/java/org/example/MediaForkJoin.java
+++ src/main/java/org/example/MediaForkJoin.java
@@ -21,12 +21,9 @@ import java.util.concurrent.RecursiveTask;
  ***************************************************************************************/
 
 
-public class MediaForkJoin extends RecursiveTask<Short>
-{
-    // private static final int UMBRAL = 10_000;     // Umbral alternativo para pruebas.
-    private static final int UMBRAL = 10_000_000;
-    private static final int LONGITUD_ARRAY = 100_000_000;
-    private static final int DUMMY_MAX = 9_999;
+public class MediaForkJoin extends RecursiveTask<Short> {
+    private static final int UMBRAL = 500;
+    private static final int LONGITUD_ARRAY = 1000;
     private short[] a_Vector = null;
     private int a_Inicio, a_Fin = 0;
 
@@ -39,30 +36,23 @@ public class MediaForkJoin extends RecursiveTask<Short>
         this.a_Vector = p_Vector;
         this.a_Inicio = p_Inicio;
         this.a_Fin = p_Fin;
-    }   // MaximoForkJoin()
-
-
-    private short getMaxRec()     // Ejecución recursiva.
-    {
-        int l_Medio = ( (a_Inicio + a_Fin) / 2 ) + 1;
-        MediaForkJoin l_Tarea1 = new MediaForkJoin(a_Vector, a_Inicio, l_Medio);;
-        MediaForkJoin l_Tarea2 = new MediaForkJoin(a_Vector, l_Medio, a_Fin);
-
-        // No tratamos el caso trivial pues cortaremos la recursividad en el UMBRAL.
-        l_Tarea1.fork();
-        l_Tarea2.fork();
-
-        return ( (short) Math.(l_Tarea1.join(), l_Tarea2.join()) );
-    }   // getMaxRec()
+    }   // MediaForkJoin()
 
     @Override
-    protected Short compute()     // Llamado por invoke() e invokeAll().
-    {
-        Short l_Retorno = 0;
-        l_Retorno = getMaxRec();   // Ejecución recursiva.
+    protected Short compute(){
+        int l_Medio = (a_Inicio + (a_Fin - a_Inicio)/2);
+        MediaForkJoin l_primeraMitad = new MediaForkJoin(a_Vector, a_Inicio, l_Medio);;
+        MediaForkJoin l_segundaMitad = new MediaForkJoin(a_Vector, l_Medio, a_Fin);
 
-        return (l_Retorno);
-    }   // compute()
+        l_primeraMitad.fork();
+        l_segundaMitad.fork();
+        Short l_primeraMitadResultado = l_primeraMitad.join();
+        Short l_segundaMitadResultado = l_segundaMitad.join();
+
+        return ((short) ((l_primeraMitadResultado+l_segundaMitadResultado)/2));
+    }   //compute()
+
+
 
 
     private short[] crearArray(int p_Longitud)
@@ -72,10 +62,7 @@ public class MediaForkJoin extends RecursiveTask<Short>
 
         for (l_Contador = 0; l_Contador < p_Longitud; l_Contador++)
         {
-            // Decide un lugar fijo donde insertar el máximo para que
-            // el tiempo de cada búsqueda en concreto no dependa de dónde esté.
-            if (l_Contador == ((short)(p_Longitud*0.9)))  l_Array[l_Contador] = DUMMY_MAX;
-            else l_Array[l_Contador] = (short) (1_000 * Math.random());
+            l_Array[l_Contador] = (short)(l_Contador);
         }
 
         return (l_Array);
@@ -98,11 +85,9 @@ public class MediaForkJoin extends RecursiveTask<Short>
         // Crea la tarea, la lanza, y obtiene el resultado "invoke".
         l_Tarea = new MediaForkJoin(l_Data, l_Inicio, l_Fin);
         l_ResultadoInvoke = l_Pool.invoke(l_Tarea);
-        // Obtención del resultado "join". 
-        l_ResultadoJoin = l_Tarea.join();
 
         System.out.println("Milisegundos empleados: " + (System.currentTimeMillis() - l_TiempoInicial));
-        System.out.println("El máximo según ‘invoke’ es: " + l_ResultadoInvoke);
+        System.out.println("Las medias según invoke son: " + l_ResultadoInvoke);
         System.out.println("Coincide con el máximo según ‘join’ que es: " + l_ResultadoJoin);
     }   // main()
 
