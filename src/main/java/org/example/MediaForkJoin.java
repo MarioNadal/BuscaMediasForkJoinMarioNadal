fdc9cdf8a5d92ba112b792c1939699b150915958
MarioNadal <220056@iessanalberto.com>
2023-11-15 09:21:58 +0100

    Finalizado sin comentarios, sin acabar cabecera

--- src/main/java/org/example/MediaForkJoin.java
+++ src/main/java/org/example/MediaForkJoin.java
@@ -10,7 +10,7 @@ import java.util.concurrent.RecursiveTask;
  ***************************************************************************************
  *  @author  M.Nadal
  *
- *  @version 1.0 -
+ *  @version 1.0 - Versión inicial del algoritmo.
  *
  *  @since   13NOV2023
  ***************************************************************************************
@@ -21,48 +21,59 @@ import java.util.concurrent.RecursiveTask;
  ***************************************************************************************/
 
 
-public class MediaForkJoin extends RecursiveTask<Short> {
+public class MediaForkJoin extends RecursiveTask<double[]> {
     private static final int UMBRAL = 500;
     private static final int LONGITUD_ARRAY = 1000;
-    private short[] a_Vector = null;
+    private double[] a_Vector = null;
     private int a_Inicio, a_Fin = 0;
 
 
     public MediaForkJoin() { }
 
 
-    public MediaForkJoin(short[] p_Vector, int p_Inicio, int p_Fin)
+    public MediaForkJoin(double[] p_Vector, int p_Inicio, int p_Fin)
     {
         this.a_Vector = p_Vector;
         this.a_Inicio = p_Inicio;
         this.a_Fin = p_Fin;
     }   // MediaForkJoin()
 
-    @Override
-    protected Short compute(){
-        int l_Medio = (a_Inicio + (a_Fin - a_Inicio)/2);
+    private double[] getMedia(){
+        int l_Medio = ((a_Fin-a_Inicio)/2);
         MediaForkJoin l_primeraMitad = new MediaForkJoin(a_Vector, a_Inicio, l_Medio);;
-        MediaForkJoin l_segundaMitad = new MediaForkJoin(a_Vector, l_Medio, a_Fin);
-
+        MediaForkJoin l_segundaMitad = new MediaForkJoin(a_Vector, l_Medio, a_Fin);l_primeraMitad.fork();
         l_primeraMitad.fork();
         l_segundaMitad.fork();
-        Short l_primeraMitadResultado = l_primeraMitad.join();
-        Short l_segundaMitadResultado = l_segundaMitad.join();
+        double[] l_primeraMitadResultado = l_primeraMitad.join();
+        double[] l_segundaMitadResultado = l_segundaMitad.join();
 
-        return ((short) ((l_primeraMitadResultado+l_segundaMitadResultado)/2));
+        return new double[]{l_primeraMitadResultado[0], l_segundaMitadResultado[0]};
+    }
+
+    @Override
+    protected double[] compute(){
+        if((a_Fin-a_Inicio) <= UMBRAL){
+            double suma = 0;
+
+            for (int i = a_Inicio; i < a_Fin; i++) {
+                suma += a_Vector[i];
+            }
+            return new double[]{(suma / (a_Fin - a_Inicio))};
+        }else{
+            double[] l_Resultado;
+            l_Resultado = getMedia();
+            return (l_Resultado);
+        }
     }   //compute()
 
-
-
-
-    private short[] crearArray(int p_Longitud)
+    private double[] crearArray(int p_Longitud)
     {
-        short[] l_Array = new short[p_Longitud];
+        double[] l_Array = new double[p_Longitud];
         int l_Contador = 0;
 
         for (l_Contador = 0; l_Contador < p_Longitud; l_Contador++)
         {
-            l_Array[l_Contador] = (short)(l_Contador);
+            l_Array[l_Contador] = (double)(l_Contador);
         }
 
         return (l_Array);
@@ -72,11 +83,10 @@ public class MediaForkJoin extends RecursiveTask<Short> {
     public static void main(String[] args)
     {
         MediaForkJoin l_Tarea = new MediaForkJoin();
-        short[] l_Data = l_Tarea.crearArray(LONGITUD_ARRAY);
+        double[] l_Data = l_Tarea.crearArray(LONGITUD_ARRAY);
         int l_Inicio = 0;
         int l_Fin = l_Data.length;
-        int l_ResultadoInvoke = 0;
-        int l_ResultadoJoin = 0;
+        double[] l_ResultadoInvoke;
         long l_TiempoInicial = System.currentTimeMillis();
         ForkJoinPool l_Pool = new ForkJoinPool();
 
@@ -87,8 +97,7 @@ public class MediaForkJoin extends RecursiveTask<Short> {
         l_ResultadoInvoke = l_Pool.invoke(l_Tarea);
 
         System.out.println("Milisegundos empleados: " + (System.currentTimeMillis() - l_TiempoInicial));
-        System.out.println("Las medias según invoke son: " + l_ResultadoInvoke);
-        System.out.println("Coincide con el máximo según ‘join’ que es: " + l_ResultadoJoin);
+        System.out.println("Las primera media es: " + l_ResultadoInvoke[0] + " y la segunda: " + l_ResultadoInvoke[1]);
     }   // main()
 
 }   // MaximoForkJoin
