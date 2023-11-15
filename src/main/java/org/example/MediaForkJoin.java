2cf5a007cbf117334d085356165d027d8bc95a96
MarioNadalCasa <220056@iessanalberto.com>
2023-11-15 17:14:21 +0100

    Finalizado 15/11/2023 Tarde

--- src/main/java/org/example/MediaForkJoin.java
+++ src/main/java/org/example/MediaForkJoin.java
@@ -1,19 +1,20 @@
 package org.example;
 
-import javax.print.attribute.standard.Media;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-/*************************************************************************************
+/***************************************************************************************
  *  APLICACIÓN: "BuscaMediasForkJoin"
  ***************************************************************************************
  *  PROGRAMACIÓN DE SERVICIOS Y PROCESOS 2DAM - IntelliJ IDEA(2023.2.5)
  ***************************************************************************************
  *  @author  M.Nadal
  *
- *  @version 1.0 - Versión inicial del algoritmo.
+ *  @version 1.1 - Versión con comentarios y excepciones
+ *           1.0 - Versión inicial del algoritmo.
  *
- *  @since   13NOV2023
+ *  @since  15NOV2023
+ *          13NOV2023
  ***************************************************************************************
  *  COMENTARIOS:
  *
@@ -25,8 +26,7 @@ import java.util.concurrent.RecursiveTask;
 public class MediaForkJoin extends RecursiveTask<Double> {
     private static final int LONGITUD_ARRAY = 1000;
     private Double[] a_Vector = null;
-    private int a_Inicio;
-    private int a_Fin = 0;
+    private int a_Inicio, a_Fin = 0;
 
 
     public MediaForkJoin() { }
@@ -39,14 +39,16 @@ public class MediaForkJoin extends RecursiveTask<Double> {
         this.a_Fin = p_Fin;
     }   // MediaForkJoin()
 
+    //Funcion que calcula la media de un array
     private Double getMedia(){
         Double suma = 0.0;
         for (int i = a_Inicio; i < a_Fin; i++) {
             suma += a_Vector[i];
         }
         return (suma/(a_Fin-a_Inicio));
-    }
+    }   //getMedia()
 
+    //Devuelve el resultado, siendo la media de el array
     @Override
     protected Double compute(){
         Double l_Resultado;
@@ -54,6 +56,7 @@ public class MediaForkJoin extends RecursiveTask<Double> {
         return (l_Resultado);
     }   //compute()
 
+    //Crea un array con los números desde 0 hasta el metido por parámetro
     private static Double[] crearArray(int p_Longitud)
     {
         Double[] l_Array = new Double[p_Longitud];
@@ -68,26 +71,36 @@ public class MediaForkJoin extends RecursiveTask<Double> {
     }   // crearArray()
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        //Crear el array total con todos los números a través de la función crearArray()
         Double[] l_Data = MediaForkJoin.crearArray(LONGITUD_ARRAY);
+        //El valor inicial
         int l_Inicio = 0;
+        //El valor final
         int l_Fin = l_Data.length;
+        //La mitad del array
         int l_Medio = (l_Inicio + (l_Fin-l_Inicio)/2);
+        //Calcula el tiempo al empezar el proceso
         long l_TiempoInicial = System.currentTimeMillis();
+        MediaForkJoin l_PrimeraTarea = null, l_SegundaTarea = null;
+        Double l_ResultadoInvokePrimeraTarea = null, l_ResultadoInvokeSegundaTarea = null;
 
         System.out.println("Inicio del cálculo.");
 
         // Crea la tarea, la lanza, y obtiene el resultado "invoke".
-        MediaForkJoin l_PrimeraTarea = new MediaForkJoin(l_Data, l_Inicio, l_Medio);
-        MediaForkJoin l_SegundaTarea = new MediaForkJoin(l_Data, l_Medio, l_Fin);
+        l_PrimeraTarea = new MediaForkJoin(l_Data, l_Inicio, l_Medio);
+        l_SegundaTarea = new MediaForkJoin(l_Data, l_Medio, l_Fin);
+        try(ForkJoinPool l_Pool = new ForkJoinPool()){
+            l_ResultadoInvokePrimeraTarea = l_Pool.invoke(l_PrimeraTarea);
+            l_ResultadoInvokeSegundaTarea = l_Pool.invoke(l_SegundaTarea);
+        }catch(SecurityException ex){
+            System.out.println("Error de seguridad en el ForkJooinPool. " + ex.getMessage());
+        }
 
-        ForkJoinPool l_Pool = new ForkJoinPool();
-        Double l_ResultadoInvokePrimeraTarea = l_Pool.invoke(l_PrimeraTarea);
-        Double l_ResultadoInvokeSegundaTarea = l_Pool.invoke(l_SegundaTarea);
-
+        //Se toma el tiempo al finalizar el proceso y restando por el tiempo iniciado, se
+        //sabrá cuanto tiempo ha tardado en total el proceso
         System.out.println("Milisegundos empleados: " + (System.currentTimeMillis() - l_TiempoInicial));
         System.out.println("La primera media es: " + l_ResultadoInvokePrimeraTarea + " y la segunda: " + l_ResultadoInvokeSegundaTarea);
     }   // main()
 
-}   // MaximoForkJoin
+}   // MediaForkJoin
